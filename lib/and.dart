152e743f9116a99fee9d74e2d1f024ef2451890a
urias perez matias <urias.perez@ulv.edu.mx>
2025-11-14 09:34:51 -0600

    Lab 3

--- lib/and.dart
+++ lib/and.dart
@@ -2,9 +2,9 @@ import 'dart:io';
 
 void and() {
   print('funcion And');
-  stdout.write("Escriba true o false");
+  stdout.write("Escriba true o false= ");
   bool a = bool.parse(stdin.readLineSync()!);
-  stdout.write("escriba true o false");
+  stdout.write("escriba true o false= ");
   bool b = bool.parse(stdin.readLineSync()!);
 
   if (a == true) {
