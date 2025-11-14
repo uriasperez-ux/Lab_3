2f021b7ed6f09e3902b5a4f4ef5e183bc97e5a78
urias perez matias <urias.perez@ulv.edu.mx>
2025-11-14 09:16:34 -0600

    Lab_3

---
+++ bin/lab_3.dart
@@ -0,0 +1,5 @@
+import 'package:lab_3/lab_3.dart' as lab_3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lab_3.calculate()}!');
+}
---
+++ test/lab_3_test.dart
@@ -0,0 +1,8 @@
+import 'package:lab_3/lab_3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
